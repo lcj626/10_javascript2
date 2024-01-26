a9ea6348a2fcb8a069f67c0919d2e953daed03db
DESKTOP-5F4QJGN\Class01 <k>
2024-01-26 18:21:42 +0900

    another commit

--- 11_REACT/04_async/src/03_data-component/04_example.js
+++ 11_REACT/04_async/src/03_data-component/04_example.js
@@ -2,9 +2,9 @@ import { useState } from "react"
 
 
 const SearchDigimon = () =>{
-  const [digimon, setDigimon] = useState("");
-  const [digimonName, setDigimonName] = useState({});
-  const [digimonImg, setDigimonImg] = useState("");
+  const [digimon, setDigimon] = useState({});
+  const [digimonName, setDigimonName] = useState("");
+  const [digimonImg, setDigimonImg] = useState([]);
 
   const onChangeHandler = (e) =>{
       setDigimon(e.target.value);
@@ -12,13 +12,11 @@ const SearchDigimon = () =>{
 
   const onClickHandler = () =>{
       fetch(`https://digi-api.com/api/v1/digimon/${digimon}`)
-      .then(r => {
-        return  r.json();
-      })
-      .then(data => {
-        console.log(data);
-        setDigimonName(data);
-        setDigimonImg(data.iamges[0].href);
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+        setDigimonName(result.name);
+        setDigimonImg(result.images[0].href);
       })
   }
 
@@ -30,7 +28,7 @@ const SearchDigimon = () =>{
           <input type="text" onChange={onChangeHandler}/>
           <button onClick={onClickHandler}>검색</button>
 
-          <h4>{`${digimonName.name}`}</h4>
+          <h4>{`${digimonName}`}</h4>
           <img src={digimonImg}/>
       </>
   )
